56ecfec34a25515583b22d76342fe863170263d6
richaar <arianna.richardson@ssfs.org>
2019-09-09 14:13:10 -0400

    Added Printing Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("hElLo wOrLd");
         System.out.println("ANOTHER LINE");
         System.out.println("This is \"Arianna's\" Computer");
+
     }
 }
