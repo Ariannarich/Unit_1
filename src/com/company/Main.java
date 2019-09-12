a5d3f4a91694d546fc4a5e7e921c6c5abdf9180e
richaar <arianna.richardson@ssfs.org>
2019-09-12 08:34:30 -0400

    Part one Exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,19 @@
 package com.company;
 
-public class Main {
+public class Main
+{
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("hElLo wOrLd");
         System.out.println("ANOTHER LINE");
         System.out.println("This is \"Arianna's\" Computer");
+
+        // Part one exercises start here:
+
+        // 1.
+        System.out.println("08:28");
+        System.out.println("09/12/2019");
 
     }
 }
