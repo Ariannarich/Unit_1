1872e739eb884c1e92af1a80a5e7ae0cfd3d0beb
richaar <arianna.richardson@ssfs.org>
2019-09-12 08:42:17 -0400

    All Parts Exercises

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -1,7 +1,28 @@
 package com.company;
 
-public class Printing {
-    public static void main(String[] args) {
+public class Printing
+{
 
-}
+    public static void main(String[] args) {
+        // Brian, I did the three commits under the Main.Java class, i'll do a full one here as well.
+        System.out.println("hElLo wOrLd");
+        System.out.println("ANOTHER LINE");
+        System.out.println("This is \"Arianna's\" Computer");
+
+        // Part one exercises start here:
+
+        // 1.
+        System.out.println("08:28");
+        System.out.println("09/12/2019");
+        // 2.
+        System.out.println("It is a great");
+        System.out.println("time to be a");
+        System.out.println("computer programmer");
+        // 3.
+        System.out.print("It is a great");
+        System.out.print(" time to be a");
+        System.out.print(" computer programmer!");
+
+
+    }
 }
