fb3facf1b2eb427401481ecf37238b80db1bfa99
richaar <arianna.richardson@ssfs.org>
2019-09-12 08:56:56 -0400

    Part II Exercise 1

---
+++ out/production/Unit_1/src/com/company/Escape.java
@@ -0,0 +1,9 @@
+package com.company;
+// Part II exercises:
+public class Escape {
+    public static void main(String[] args) {
+        // 4.
+        System.out.println("What is the difference between");
+        System.out.println("a");
+    }
+}
