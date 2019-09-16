1f6c7640dd17d9c1ea4bcb099e263ec58464dd0a
richaar <arianna.richardson@ssfs.org>
2019-09-16 11:06:11 -0400

    1.3 exercise B

---
+++ out/production/Unit_1/src/com/company/PoundsToKilograms.java
@@ -0,0 +1,11 @@
+// Pounds to kilograms exercise
+package com.company;
+
+public class PoundsToKilograms {
+    public static void main(String[] args)
+    {
+  double lbs = 115;
+  double kg =  lbs /2.205;
+        System.out.println("115.0 pounds = " + kg);
+    }
+}
