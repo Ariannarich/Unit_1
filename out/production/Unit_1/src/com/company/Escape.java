0f2f23a16a50d2ce6eee968660d0c4ac879fba66
richaar <arianna.richardson@ssfs.org>
2019-09-17 08:02:29 -0400

    Making up earlier parts(Escape and Meta)

--- out/production/Unit_1/src/com/company/Escape.java
+++ out/production/Unit_1/src/com/company/Escape.java
@@ -4,13 +4,17 @@ public class Escape {
     public static void main(String[] args) {
         // 4.
         System.out.println("What is the difference between");
-        System.out.println("a ' and a \"? or between a \"?   ");
+        System.out.println("a ' and a \"? or between a \\\"?");
         System.out.println("One is what we see when we're typing our program.");
         System.out.println("The other is what appears on the \"console.\"");
 
         //5.
-        System.out.println("\\/");
-        System.out.println("\\//");
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
     }
 
 }
