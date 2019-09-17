93f9c12519f0ebff2f3b64eb4c176a90936c9713
richaar <arianna.richardson@ssfs.org>
2019-09-17 12:40:03 -0400

    TempConversion

---
+++ out/production/Unit_1/src/com/company/TempConversion.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TempConversion {
+    public static void main(String[] args)
+    {
+        int fahrenheit = 75;
+                int celsius = (fahrenheit - 32) * 5/9;
+System.out.println("Fahrenheit: " + fahrenheit);
+System.out.println("Celsius: " + celsius);
+    }
+}
