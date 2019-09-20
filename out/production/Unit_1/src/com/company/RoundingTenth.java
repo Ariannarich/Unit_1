0cc1067e9dcf6b5d214bd74515aba08ece086e5f
Ariannarich <arianna.richardson@ssfs.org>
2019-09-20 01:14:22 -0400

    Rounding Tenth

---
+++ out/production/Unit_1/src/com/company/RoundingTenth.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+    public class RoundingTenth {
+        public static void main(String[] arg)
+        {
+
+            System.out.print("Enter your number here: ");
+            Scanner value= new Scanner(System.in);
+            double input;
+            input = value.nextDouble();
+            double num1 = input * 10;
+            double num2 = num1 + 0.5;
+          double temp2 = (int) num2;
+            double answer = temp2/ 10;
+
+            System.out.println( (input ) + " rounded to the nearest tenth: " + answer);
+
+        }
+    }
+
+
+
