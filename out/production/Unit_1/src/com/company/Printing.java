85abe050e3e413288ad63336cfed7df368b8bb6a
Ariannarich <arianna.richardson@ssfs.org>
2019-09-24 09:10:09 -0400

    Correctchange

--- out/production/Unit_1/src/com/company/Printing.java
+++ out/production/Unit_1/src/com/company/Printing.java
@@ -7,7 +7,8 @@ public class Printing
         // Brian, I did the three commits under the Main.Java class, i'll do a full one here as well.
         System.out.println("hElLo wOrLd");
         System.out.println("ANOTHER LINE");
-        System.out.println("This is \"Arianna's\" Computer");
+        double test = 3.75 % 3;
+        System.out.println(test);
 
         // Part one exercises start here:
 
